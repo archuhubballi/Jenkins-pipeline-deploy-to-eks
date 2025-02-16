17c02e751c2dd55f0d0bacc14572c6847421adc7
archuhubballi <archuhubballi907@gmail.com>
2025-02-16 17:54:54 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
       bucket = "mydevops-archuhubballi-s3bucket"
-      region = "us-east-2"
+      region = "us-east-1"
       key = "Archu/terraform.tfstate"
     }
 }
