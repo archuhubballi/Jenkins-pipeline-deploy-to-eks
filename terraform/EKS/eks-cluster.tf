ccdaba01b5906f5b4ea0a1176fe81357a9157222
archuhubballi <archuhubballi907@gmail.com>
2025-02-16 18:48:54 +0530

    adding eks cluster configuration file

---
+++ terraform/EKS/eks-cluster.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  region = "us-west-2"  # Change to your AWS region
+}
+
+# Create IAM Role for the EKS Cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach policies to the EKS Cluster IAM Role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+# Create EKS Cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "simple-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = ["PublicSubnet", "PublicSubnet"]  # Replace with your VPC subnet IDs
+  }
+}
+
+# Security group for EKS
+resource "aws_security_group" "eks_sec_group" {
+  name        = "eks-sec-group"
+  description = "EKS Security Group"
+  vpc_id      = "myvpc"  # Replace with your VPC ID
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "eks_cluster_certificate_authority_data" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
